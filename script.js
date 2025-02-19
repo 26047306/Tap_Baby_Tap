6c7796bb913e0c0bab24b6d6245570446d938c90
Sandeep Yadav <168341537+26047306@users.noreply.github.com>
2025-02-19 21:52:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const gameContainer = document.getElementById('game-container');
+let score = 0;
+
+function createCircle() {
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    circle.style.left = 
+    Math.random() * (gameContainer.offsetWidth - 50) + 'px';
+
+    circle.style.top = 
+    Math.random() * (gameContainer.offsetHeight - 50) + 'px';
+    
+    gameContainer.appendChild(circle);
+
+    circle.addEventListener('click', () => {         
+        score++;
+        updateScore();
+        gameContainer.removeChild(circle);
+    });
+
+    setTimeout(() => gameContainer.removeChild(circle), 1000);
+    }
+
+function updateScore() {
+    document.getElementById('score').textContent = 'Score: ' + score;
+    }
+start.addEventListener('click',()=>{
+    setInterval(createCircle, 500);
+})
+    updateScore();
